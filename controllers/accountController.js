145fe1373691c93a42e5c766f47893838e4c01b2
Sakhile Mamba <sakhymamba@gmail.com>
2024-10-18 00:56:51 +0200

    client side form data validation, acoount registration functionality

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,4 +1,5 @@
 const utilities = require("../utilities")
+const accountModel = require("../models/account-model")
 
 
 /* ****************************************
@@ -27,4 +28,26 @@ async function buildRegister(req, res, next) {
     })
 }
 
-module.exports = { buildLogin, buildRegister }
+
+/* ****************************************
+*  Process Registration
+* *************************************** */
+async function registerAccount(req, res) {
+    let nav = await utilities.getNav()
+    const error = await utilities.buildError()
+    const { account_firstname, account_lastname, account_email, account_password } = req.body
+
+    const regResult = await accountModel.registerAccount(account_firstname, account_lastname, account_email, account_password)
+
+    if (regResult) {
+        req.flash("notice", `Congratulations, you\'re registered ${account_firstname}. Please log in`)
+        res.status(201).render("account/login", { title: "Login", nav, error })
+    } else {
+        req.flash("notice", "Sorry the registration failed.")
+        res.status(501).render("account/register", { title: "Registration", nav, error })
+    }
+
+}
+
+
+module.exports = { buildLogin, buildRegister, registerAccount }
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -5,9 +5,12 @@ const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
 
 //Route to build login view
-router.get("/login", utilities.handleErrors(accountController.buildLogin));
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
 
-//Route to build a register view
-router.get("/register", utilities.handleErrors(accountController.buildRegister));
+//Route to build a register 
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+//Route to process the registration of a new account
+router.post("/register", utilities.handleErrors(accountController.registerAccount))
 
 module.exports = router;
